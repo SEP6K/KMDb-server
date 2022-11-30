30439f28f41abd5b0904182264a011710d4cf40b
weilderbeast <bogdan.is.a.beast@gmail.com>
2022-11-30 14:14:14 +0100

    remove prisma for testing

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,9 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
-  const prisma = new PrismaClient();
-  const movie = await prisma.movies
-    .findFirst({
-      where: {
-        id: 15724,
-      },
-    })
-    .then((res) => {
-      console.log(res.title);
-      return res.title;
-    });
   res.send("hello world");
 });
 
