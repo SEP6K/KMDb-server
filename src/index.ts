83c2af981b490e47553d96a0d08e86a2da986fc6
weilderbeast <bogdan.is.a.beast@gmail.com>
2022-11-30 13:31:15 +0100

    simple query

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.get("/", async (req, res) => {
+  const prisma = new PrismaClient();
+  const movie = await prisma.movies.findFirst({
+    where: {
+      id: 15724,
+    },
+  });
+  res.send(movie);
 });
 
 app.listen(port, () => {
