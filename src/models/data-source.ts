087a47c93c401b3dab4bcae2bea590055a936ff4
IonutGrosu <ionutflorentin.grosu@gmail.com>
2022-12-07 15:59:48 +0100

    tried smth new

--- src/models/data-source.ts
+++ src/models/data-source.ts
@@ -1,19 +1,18 @@
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import { DataSource } from "typeorm";
 import { Directors, Movies, People, Ratings, Stars } from "./models.js";
 
 dotenv.config({
   debug: true,
-  path: "../../.env",
 });
 
 const dataSource = new DataSource({
   type: "postgres",
-  host: "34.159.156.47",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT!),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [Directors, Movies, People, Ratings, Stars],
 });
 
