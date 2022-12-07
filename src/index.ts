2c3861e478f787380505ea50ccaac791895a80cc
florinaToldea <299116@via.dk>
2022-12-07 12:50:36 +0100

    querying

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,43 @@ import express from "express";
 import dotenv from "dotenv";
 import "reflect-metadata";
 import { connection } from "./models/data-source.js";
+import { Movies } from "./models/models.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", async (req, res) => {
+app.get("/movie/year", async (req, res) => {
   console.log(process.env.DB_HOST);
   connection.then((ds) => {
-    // const query = createQueryBuilder("movies").getMany();
-    const movieRepo = ds.getRepository("Movies");
+    const movieRepo = ds.getRepository(Movies);
 
     movieRepo.findOneBy({ movie_id: 15414 }).then((res) => {
       console.log(res);
     });
+    movieRepo
+      .createQueryBuilder("movie")
+      .where("movie.year = :year", { year: 1999 })
+      .getMany()
+      .then((res) => {
+        console.log(res);
+      });
+  });
+});
+
+app.get("/movie/title", async (req, res) => {
+  const title = req.query.title;
+  connection.then((ds) => {
+    const movieRepo = ds.getRepository(Movies);
+
+    movieRepo
+      .createQueryBuilder("movie")
+      .where("movie.title LIKE :title", { title: `%${title}%` })
+      .take(5)
+      .getMany()
+      .then((res) => {
+        console.log(res);
+      });
   });
 });
 
