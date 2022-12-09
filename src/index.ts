6835eed584988be81515594260785ff43f7a442e
IonutGrosu <ionutflorentin.grosu@gmail.com>
2022-12-09 10:48:41 +0100

    finished feature

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import * as dotenv from "dotenv";
 import express from "express";
 import "reflect-metadata";
 import { connection } from "./models/data-source.js";
 import { Movies } from "./models/models.js";
-dotenv.config({
-  debug: true,
-});
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,36 +11,55 @@ app.get("/", async (req, res) => {
   res.send("Hello world");
 });
 
-app.get("/movie/year", async (req, res) => {
-  console.log(process.env.DB_HOST);
+app.get("/movie", async (req, res) => {
+  const titleQuery = req.query.title;
+  console.log(titleQuery);
+  const yearQuery = req.query.year;
+  console.log(yearQuery);
+
   connection.then((ds) => {
     const movieRepo = ds.getRepository(Movies);
 
-    movieRepo
-      .createQueryBuilder("movie")
-      .where("movie.year = :year", { year: 1999 })
-      .getMany()
-      .then((movies) => {
-        console.log(movies);
-        res.send(movies);
-      });
-  });
-});
-
-app.get("/movie/title", async (req, res) => {
-  const title = req.query.title;
-  connection.then((ds) => {
-    const movieRepo = ds.getRepository(Movies);
-
-    movieRepo
-      .createQueryBuilder("movie")
-      .where("movie.title LIKE :title", { title: `%${title}%` })
-      .take(5)
-      .getMany()
-      .then((movies) => {
-        console.log(movies);
-        res.send(movies);
-      });
+    if (titleQuery) {
+      if (yearQuery) {
+        // query both by year and title
+        movieRepo
+          .createQueryBuilder("movie")
+          .where("movie.title LIKE :title AND movie.year = :year", {
+            title: `%${titleQuery}%`,
+            year: yearQuery,
+          })
+          .take(5)
+          .getMany()
+          .then((movies) => {
+            console.log(movies);
+            res.send(movies);
+          });
+      } else {
+        // query only by title
+        movieRepo
+          .createQueryBuilder("movie")
+          .where("movie.title LIKE :title", { title: `%${titleQuery}%` })
+          .take(5)
+          .getMany()
+          .then((movies) => {
+            console.log(movies);
+            res.send(movies);
+          });
+      }
+    } else {
+      if (yearQuery) {
+        // query only by year
+        movieRepo
+          .createQueryBuilder("movie")
+          .where("movie.year = :year", { year: yearQuery })
+          .getMany()
+          .then((movies) => {
+            console.log(movies);
+            res.send(movies);
+          });
+      }
+    }
   });
 });
 
